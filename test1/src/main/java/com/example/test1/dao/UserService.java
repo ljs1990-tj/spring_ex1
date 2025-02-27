8e8e32a286799b9174cc875540cce44c5dff2858
ljs1990-tj <ljs1990.tj@gmail.com>
2025-02-27 09:43:33 +0900

    2025.02.27 spring 초기 버전

---
+++ test1/src/main/java/com/example/test1/dao/UserService.java
@@ -0,0 +1,44 @@
+package com.example.test1.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.test1.mapper.UserMapper;
+import com.example.test1.model.Member;
+import com.example.test1.model.User;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	public HashMap<String, Object> userLogin(HashMap<String, Object> map) {
+		// TODO Auto-generated method stub
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		
+		User user = userMapper.userMapperLogin(map);
+		String result = user != null ? "success" : "fail";
+		
+		resultMap.put("result", result);
+		resultMap.put("info", user); 
+		
+		return resultMap;
+	}
+
+	public HashMap<String, Object> memberList(HashMap<String, Object> map) {
+		// TODO Auto-generated method stub
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		List<Member> list = userMapper.getUserList(map);
+		resultMap.put("list", list); 
+		// resultMap : { list : [ {},{},{}] }
+		return resultMap;
+	}
+}
+
+
+
+
